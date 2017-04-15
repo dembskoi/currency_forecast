32cc4b7ffc70f5cecad3fa6234fb74f2200e1e7f
Aleksandr Dembskii <dembskoi@gmail.com>
2017-04-15 17:34:10 +0300

    Disable unnesessary devise options

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+  as :user do
+    get '/sign_up' => 'devise/registrations#new', as: 'new_user_registration'
+    post 'users' => 'devise/registrations#create', as: 'user_registration'
+  end
   resources :users
 end
