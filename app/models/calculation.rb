0b077ead681aacd7f6aacaa5936d905b84c73688
Aleksandr Dembskii <dembskoi@gmail.com>
2017-04-20 21:28:39 +0300

    Amount numericality validation

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -1,4 +1,8 @@
 class Calculation < ApplicationRecord
   belongs_to :base_currency, class_name: 'Currency'
   belongs_to :target_currency, class_name: 'Currency'
+
+  # Max PostgreSQL integer is 4 bytes	= 2147483647
+  validates :amount, presence: true,
+            numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 2147483647 }
 end
